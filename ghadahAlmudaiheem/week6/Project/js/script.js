6d2872ce95643510a68786b39ea4038489fe9d6c
ghadamud <56229366+ghadamud@users.noreply.github.com>
2019-11-18 16:16:22 +0300

    first darft of project

---
+++ ghadahAlmudaiheem/week6/Project/js/script.js
@@ -0,0 +1,13 @@
+// $('.secondDiv').flickity({
+//     // options
+//     autoPlay: 4000,
+//     cellAlign: 'left',
+//     contain: true,
+//     setGallerySize: false,
+//     wrapAround: true
+//   });
+$('#listItem1').click(function(){
+  
+    $.scrollTo('', 1000);
+  });
+  
